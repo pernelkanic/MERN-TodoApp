a342388e6a21c25301f301434d4c855d46384dc1
pernelkanic <211001118@rajalakshmi.edu.in>
2023-06-06 21:16:40 +0530

    cddd

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar=()=>{
         <header>
         <div className="container">
             <Link to ='/'>
-                <h1>Todoo Appppp</h1>
+                <h1>Todoo Apppppp</h1>
             </Link>
         <nav>
             {user && (
